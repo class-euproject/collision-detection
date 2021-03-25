9ad7e5f6d8bb95acbdf98ae40396b6d8fa79c9a1
kpavel <kpavel@il.ibm.com>
2021-03-25 14:15:02 +0000

    wip

--- cdLithopsRunner.py
+++ cdLithopsRunner.py
@@ -1,3 +1,5 @@
+print("before imports")
+
 import os 
 import time
 
@@ -8,14 +10,16 @@ from cd.CD import collision_detection
 from geolib import geohash
 import paho.mqtt.client as mqtt
 
-# needed for debugging
-BEGIN = time.time()
-start = BEGIN
+print("after imports")
 
-def timeConsumed(name):
-    global start
-    print(f'{name} time: {time.time() - start}, current time: {time.time()}')
-    start = time.time()
+# needed for debugging
+#BEGIN = time.time()
+#start = BEGIN
+
+#def timeConsumed(name):
+#    global start
+#    print(f'{name} time: {time.time() - start}, current time: {time.time()}')
+#    start = time.time()
 ##############
 
 # either return from dataclay only relevant data (to optimize times)
@@ -23,9 +27,9 @@ def timeConsumed(name):
 def _is_collided(main_object, other_object):
     res = []
     if main_object[0] and main_object[1] and main_object[2] and other_object[0] and other_object[1] and other_object[2] and min(main_object[0])>-180 and max(main_object[0])<180 and min(other_object[0])>-180 and max(other_object[0])<180 and min(main_object[1])>-90 and min(other_object[1])>-90 and max(main_object[0])<90 and max(other_object[1])<90 and (max(main_object[0])-min(main_object[0]))<2 and (max(main_object[1])-min(main_object[1]))<2 and (max(other_object[0])-min(other_object[0]))<2 and (max(other_object[1])-min(other_object[1]))<2:
-#        print("before collision_detection(main_object,other_object)")
-        res = collision_detection(('n',) + main_object,('n',) + other_object)
-#        print("after collision_detection(main_object,other_object)")
+        print(f"before call collision_detection={main_object[4]}:{other_object[4]}")
+        res = collision_detection(main_object, other_object)
+        print(f"after call collision_detection={main_object[4]}:{other_object[4]}")
     return res
 
 def _getConnectedCarsInWA(my_object, connected_cars_objects):
@@ -40,15 +44,19 @@ def _getConnectedCarsInWA(my_object, connected_cars_objects):
 # how the result should be processed?
 def detect_collision(objects_chunk, connected_cars):
     res = []
+    print(f"PAIRS_NUM:{len(objects_chunk) * len(connected_cars)}")
     for my_object in objects_chunk:
-      print(f"in detect collision with {my_object}")
+#      print(f"in detect collision with {my_object}")
       cc_in_wa = _getConnectedCarsInWA(my_object, connected_cars)
 
       for cc in cc_in_wa:
         start = time.time()
         collisions = _is_collided(my_object, cc)
         if collisions:
-            print(">>> Collision with connected car {} detected".format(cc))
+            my_id = my_object[4]
+            ccid = cc[4]
+
+            print(f"Collision detected, before mqtt={my_id}:{ccid}")
             time_detected = time.time()
             client=mqtt.Client()
             client.connect("192.168.7.42")
@@ -59,16 +67,14 @@ def detect_collision(objects_chunk, connected_cars):
 #            my_object += (my_obj.id_object,)
 #            cc += (cc_obj.id_object,)
             
-            my_id = my_object[4]
-            ccid = cc[4]
 #            _my_object = my_object[1:5]
 #            _cc = cc[1:5]
             client.publish("test",f"{my_id} {ccid} {collisions} {my_object[0]} {my_object[1]} {my_object[2]} {cc[0]} {cc[1]} {cc[2]}")
+            print(f"Collision detected, after mqtt={my_id}:{ccid}")
 #            client.publish("test","Collision of {} with connected car {} detected, COLLISION DATA {} ==== TRAJ1 {} ==== TRAJ2 {} ==== obj1 {} === obj2 {}".format(my_obj.id_object, cc_obj.id_object, collisions, cc, my_object, cc_obj, my_obj))
 
             # here will be push to car mqtt topic
-            if ccid not in res:
-                res.append((my_id, ccid, collisions))
+            res.append((my_id, ccid, collisions))
 
     return res
 
@@ -83,14 +89,15 @@ def acquireLock(REDIS_HOST):
     import redis
     redis_client = redis.StrictRedis(host=REDIS_HOST,port=6379)
     for i in range(CONCURRENT_CD):
-        lock = redis_client.lock(f'cdlock{i}', 30, 0.1, 0.01)
+        lock = redis_client.lock(f'cdlock{i}', 60, 0.1, 0.01)
         if lock.acquire():
             return lock
     return None
 
 def run(params=[]):
+    print("in run")
 
-    timeConsumed("start")
+#    timeConsumed("start")
 
     from cd.dataclayObjectManager import DataclayObjectManager
 
@@ -98,20 +105,21 @@ def run(params=[]):
     if not lock:
         return {'error': f'There currently maximum number of {CONCURRENT_CD} simulatiously running CD actions'}
 
-    timeConsumed("dataclay.init")   #TODO: to be removed. needed for debugging
+#    timeConsumed("dataclay.init")   #TODO: to be removed. needed for debugging
 
     fexec = lithops.FunctionExecutor(log_level='DEBUG')
 
-    timeConsumed("executor")   #TODO: to be removed. needed for debugging
+#    timeConsumed("executor")   #TODO: to be removed. needed for debugging
     if 'ALIAS' not in params:  #TODO: to be removed. needed for debugging
         print("Params %s missing ALIAS parameter" % params)
         exit()
 
     alias = params['ALIAS']
-    print("ALIAS: %s" % alias)
+#    print("ALIAS: %s" % alias)
 
+    print("creating dm instance")
     dm = DataclayObjectManager(alias=alias)
-    timeConsumed("DataclayVehicleManager")   #TODO: to be removed. needed for debugging
+#    timeConsumed("DataclayVehicleManager")   #TODO: to be removed. needed for debugging
 
     limit = None
     if 'LIMIT' in params and params['LIMIT'] != None:  #TODO: to be removed. needed for debugging
@@ -123,7 +131,8 @@ def run(params=[]):
 
 #    import pdb;pdb.set_trace()
     objects = dm.getAllObjects(with_tp=True, with_event_history=False)
-    timeConsumed("dm.getAllObjects")
+    print("after dm.getAllObjects")
+#    timeConsumed("dm.getAllObjects")
 
     if limit:
         objects = getLimitedNumberOfObjects(objects, limit)
@@ -162,7 +171,7 @@ def run(params=[]):
         else:
             connected_cars = objects
 #    import pdb;pdb.set_trace()
-    timeConsumed("connected_cars = dm.getObjectTuplesWithTp")
+#    timeConsumed("connected_cars = dm.getObjectTuplesWithTp")
 
     kwargs = []
     res = []
@@ -171,30 +180,35 @@ def run(params=[]):
       for objects_chunk in chunker(objects, chunk_size):
         kwargs.append({'objects_chunk': objects_chunk, 'connected_cars': connected_cars})
 
-      timeConsumed("kwargs.append for {} number of objects".format(len(objects)))
+      #timeConsumed("kwargs.append for {} number of objects".format(len(objects)))
 
 #      import pdb;pdb.set_trace()
+      print("before lithops fexec.map")
       fexec.map(detect_collision, kwargs, extra_env = {'__LITHOPS_LOCAL_EXECUTION': True})
-      timeConsumed("fexec.map")
+
+#      timeConsumed("fexec.map")
 
 #    pw.wait(download_results=False, WAIT_DUR_SEC=0.015)
+      print("after lithops fexec.map")
       if objects:
         res = fexec.get_result(WAIT_DUR_SEC=0.015)
 
+    print("lithops finished")
     client=mqtt.Client()
     client.connect("192.168.7.42")
     topic = "cd-out"
     client.publish(topic,f"CD finished")
 
     print("results: {}".format(res))
-    timeConsumed("fexec.wait")
+#    timeConsumed("fexec.wait")
 
     lock.release()
 
+    print("returning from lithops function")
     return {"finished": "true"}
 
 
-REDIS_HOST = '10.101.88.224'
+REDIS_HOST = '10.97.50.24'
 
 if __name__ == '__main__':
     import sys
